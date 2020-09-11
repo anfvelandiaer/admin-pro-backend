a47016b3e1f346b27c20cbf64dcf4213ef7a07ce
anfvelandiaer <anfvelandiaer@unal.edu.co>
2020-09-10 22:42:23 -0500

    sección 12 - google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const { response } = require("express");
 const bcrypt = require('bcryptjs')
 const { generarJWT } = require('../helpers/jwt')
+const { googleVerify } = require("../helpers/google-verify");
 const Usuario = require("../models/usuario");
+const { db } = require("../models/usuario");
 
 
 const login = async (req, res = response) => {
@@ -45,6 +47,54 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async(req, res=response) => {
+
+  const googleToken = req.body.token;
+  
+
+  try {
+    const { name, email, picture } = await googleVerify(googleToken)
+
+    //Verificar email
+    const usuarioDB = await Usuario.findOne({email});
+    let usuario;
+    if ( !usuarioDB ) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: '@@@',
+        img: picture,
+        google: true
+      })
+    } else {
+      // Existe usuario
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    //guardar en DB
+
+    await usuario.save();
+
+    //Generar el Token -JWT
+    const token = await generarJWT( usuario.id);
+
+
+    res.json({
+      ok:true,
+      token
+    })
+  } catch (error) {
+
+    res.status(401).json({
+      ok:false,
+      msg: 'El token no es correcto'
+    })
+  }
+
+}
+
 module.exports = {
   login,
+  googleSignIn
 };
---
+++ helpers/google-verify.js
@@ -0,0 +1,21 @@
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_ID);
+
+
+const googleVerify = async( token ) => {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_ID, 
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  const { name, email, picture } = payload;
+
+  return { name, email, picture };
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+
+module.exports = {
+    googleVerify
+}
